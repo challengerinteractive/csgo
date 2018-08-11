0836dd1bc12d4d832257f51575338240b676135b
Patrick McClory <pmdev@mbp13.home.mcclory.io>
2018-08-10 17:01:37 -0700

    new start.sh file for gameserver with logging and shit

--- gameserver/containerfs/csgo/start.sh
+++ gameserver/containerfs/csgo/start.sh
@@ -12,7 +12,21 @@ export GAME_MODE="${GAME_MODE:-1}"
 export MAP="${MAP:-de_dust2}"
 export MAPGROUP="${MAPGROUP:-mg_active}"
 export MAXPLAYERS="${MAXPLAYERS:-12}"
+export FRIENDLY_FIRE="${FRIENDLY_FIRE:1}"
+export SERVER_TAGS="ChallengerVault"
 
+#Value	Location
+#0	US - East
+#1	US - West
+#2	South America
+#3	Europe
+#4	Asia
+#5	Australia
+#6	Middle East
+#7	Africa
+#255	World (default)
+
+export REGION_NUMBER="${REGION_NUMBER:1}"
 
 export CSGO_DIR="${CSGO_DIR:-/csgo}"
 
@@ -21,9 +35,29 @@ cd $CSGO_DIR
 ### Create dynamic server config
 cat << SERVERCFG > $CSGO_DIR/csgo/cfg/server.cfg
 hostname "$SERVER_HOSTNAME"
-rcon_password "$RCON_PASSWORD"
+rcon_password ""
 sv_lan 0
 sv_cheats 0
+writeid
+writeip
+sv_allow_votes “1″ //Turns server voting on and off.
+sv_vote_allow_spectators “0″ //Allow spectators to vote?”
+sv_vote_command_delay “2″ //How long after a vote passes until the action happens
+sv_vote_creation_time “120″ //How often someone can individually call a vote.
+sv_vote_failure_timer “300″ //A vote that fails cannot be re-submitted for this long
+sv_vote_quorum_ratio “0″ //The minimum ratio of players needed to vote on an issue to resolve it.
+sv_vote_timer_duration “15″ //How long to allow voting on an issue
+log on
+mp_logfile 1
+mp_logdetail 3
+mp_logmessages 1
+sv_log_onefile “1″ //Log server information to only one file.
+sv_logbans “1″ //Log server bans in the server logs.
+sv_logecho “1″ //Echo log information to the console.
+sv_logfile “1″ //Log server information in the log file.
+sv_logflush “0″ //Flush the log file to disk on each write (slow).
+mp_friendlyfire “$FRIENDLY_FIRE″ //Enable Friendly Fire 1 =Enable 0 =Disable
+sv_tags "$SERVER_TAGS"
 SERVERCFG
 
 ./srcds_run \
@@ -39,4 +73,5 @@ SERVERCFG
     +mapgroup $MAPGROUP \
     +map $MAP \
     +ip $IP \
-    +sv_setsteamaccount $STEAM_ACCOUNT
+    +sv_setsteamaccount $STEAM_ACCOUNT \
+    +rcon_password $RCON_PASSWORD
