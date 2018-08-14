26c980c93ddf154a132db0734fe99a1f268db1a7
Patrick McClory <patrick@mcclory.io>
2018-08-14 08:48:54 -0700

    [skip ci]

--- ubuntu-ec2-bootstrap.sh
+++ ubuntu-ec2-bootstrap.sh
@@ -3,7 +3,7 @@ sudo mkdir -p /var/lib/docker
 sudo mkfs -t ext4 /dev/nvme1n1
 sudo mount /dev/nvme1n1 /var/lib/docker 
 sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common python3-pip
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
