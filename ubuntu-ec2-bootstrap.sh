161c85f9adc185812460c477599d642bb1baa24d
Patrick McClory <patrick@mcclory.io>
2018-08-14 08:49:29 -0700

    [skip ci]

--- ubuntu-ec2-bootstrap.sh
+++ ubuntu-ec2-bootstrap.sh
@@ -4,6 +4,7 @@ sudo mkfs -t ext4 /dev/nvme1n1
 sudo mount /dev/nvme1n1 /var/lib/docker 
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common python3-pip
+sudo pip3 install awscli
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
