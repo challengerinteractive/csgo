64ba0befe9741b808fc8bb2f739ac90e895d3b9b
Patrick McClory <patrick@mcclory.io>
2018-08-31 07:24:11 -0700

    [skip ci] fixing for ssh host checking

--- run_redeploy_dev.sh
+++ run_redeploy_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh -i ~/.ssh/deploy_rsa ubuntu@ec2-54-200-35-148.us-west-2.compute.amazonaws.com "./redeploy.sh" &
-ssh -i ~/.ssh/deploy_rsa ubuntu@ec2-52-33-177-125.us-west-2.compute.amazonaws.com "./redeploy.sh" &
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/deploy_rsa ubuntu@ec2-54-200-35-148.us-west-2.compute.amazonaws.com "./redeploy.sh" &
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/deploy_rsa ubuntu@ec2-52-33-177-125.us-west-2.compute.amazonaws.com "./redeploy.sh" &
 wait
