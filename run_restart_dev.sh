faa373b757bcc27cca1269017c43c4ee41d7fff1
Patrick McClory <pmdev@introspectdata.com>
2018-09-01 09:01:19 -0700

    updating with wait... thanks patrick

--- run_restart_dev.sh
+++ run_restart_dev.sh
@@ -7,6 +7,7 @@ echo "**************************************************************************
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ubuntu@ec2-54-200-35-148.us-west-2.compute.amazonaws.com "docker-compose down && docker-compose up -d && timeout 30 docker-compose logs -f" &
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ubuntu@ec2-52-33-177-125.us-west-2.compute.amazonaws.com "docker-compose down && docker-compose up -d && timeout 30 docker-compose logs -f" &
+wait
 
 echo "********************************************************************************"
 echo "*"
--- run_restart_prod.sh
+++ run_restart_prod.sh
@@ -9,6 +9,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ub
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ubuntu@ec2-35-165-84-140.us-west-2.compute.amazonaws.com "docker-compose down && docker-compose up -d" &
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ubuntu@ec2-18-232-142-175.compute-1.amazonaws.com "docker-compose down && docker-compose up -d" &
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i deploy_rsa ubuntu@ec2-34-230-60-12.compute-1.amazonaws.com "docker-compose down && docker-compose up -d" &
+wait
 
 echo "********************************************************************************"
 echo "*"
